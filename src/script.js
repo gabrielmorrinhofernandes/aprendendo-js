587f04844108da21745320a095cbe55b8fd67d1b
gabrielmorrinhofernandes <gabrielmorrinhofernandes@gmail.com>
2026-07-30 09:07:58 -0300

    jogo RPS

---
+++ src/script.js
@@ -0,0 +1,10 @@
+let rng;
+let playerChoice, botChoice;
+let nome;
+let menu;
+    if(playerChoice === 'pedra' & botChoice === 'tesoura' || playerChoice === 'papel' & botChoice === 'pedra' || playerChoice === 'tesoura' & botChoice === 'papel'){
+        document.getElementById('resultado').innerText = "ganhou";
+    }
+    else if(playerChoice === 'pedra' & botChoice === 'pedra' || playerChoice === 'papel' & botChoice === 'papel' || playerChoice === 'tesoura' & botChoice === 'tesoura'){
+        
+    }
